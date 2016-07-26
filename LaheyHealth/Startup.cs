e0ab0767288a895c84dc2cd72f962eb9fbfdd828
nicocava <nicocava92@gmail.com>
2016-07-26 10:46:05 -0300

    Initial commit

---
+++ LaheyHealth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LaheyHealth.Startup))]
+namespace LaheyHealth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
